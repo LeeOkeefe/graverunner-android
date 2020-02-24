abaf09404c97b2d0b9bcd87e50e50b309d4fba71
LSO107 <34274877+LSO107@users.noreply.github.com>
2020-02-24 18:04:49 +0000

    Main Menu

---
+++ Assets/Scripts/UI/MenuManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace UI
+{
+    internal sealed class MenuManager : MonoBehaviour
+    {
+        public void PlayGame()
+        {
+            SceneManager.LoadScene("Game");
+        }
+
+        public void QuitGame()
+        {
+#if UNITY_EDITOR
+            EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+#endif
+        }
+    }
+}
