71ed7c5363ee0545b5c494e950475673e1e79261
LSO107 <34274877+LSO107@users.noreply.github.com>
2020-02-24 10:57:32 +0000

    Camera Shake Fix

--- Assets/Scripts/Extensions/CameraExtensions.cs
+++ Assets/Scripts/Extensions/CameraExtensions.cs
@@ -1,28 +1,12 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace Extensions
 {
     internal static class CameraExtensions
     {
-        public static IEnumerator Shake(this Camera camera, float magnitude, float duration)
+        public static void Shake(this Camera camera)
         {
-            var cameraTransform = camera.transform;
-            var orignalPosition = cameraTransform.position;
-            var elapsed = 0f;
-
-            while (elapsed < duration)
-            {
-                var x = Random.Range(-1f, 1f) * magnitude;
-                var y = Random.Range(-1f, 1f) * magnitude;
-
-                cameraTransform.position = new Vector3(x, y, -1f);
-                elapsed += Time.deltaTime;
-                yield return 0;
-            }
-
-            cameraTransform.position = orignalPosition;
+            camera.GetComponentInParent<Animation>().Play();
         }
     }
 }
