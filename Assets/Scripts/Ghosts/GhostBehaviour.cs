3dd17b3fd83b9ded46d809bdf5a0f9ed33ef0650
LSO107 <34274877+LSO107@users.noreply.github.com>
2020-02-28 19:59:58 +0000

    Ghost Death
    behaviour deactivation on hit
    ghost fade
    destroy ghost at the end of the animation using anim event

--- Assets/Scripts/Ghosts/GhostBehaviour.cs
+++ Assets/Scripts/Ghosts/GhostBehaviour.cs
@@ -13,12 +13,18 @@ namespace Ghosts
         [SerializeField] private float m_MoveSpeed = 1f;
 
         private Vector3 m_TargetPos;
+
         private Collider2D m_Collider;
+        private Animator m_Anim;
+        private bool m_IsAlive = true;
+
+        private static readonly int Death = Animator.StringToHash("Death");
 
         private void Awake()
         {
             m_SpriteRenderer = GetComponent<SpriteRenderer>();
             m_Collider = GetComponent<Collider2D>();
+            m_Anim = GetComponent<Animator>();
         }
 
         private void Start()
@@ -29,6 +35,9 @@ namespace Ghosts
 
         private void Update()
         {
+            if (!m_IsAlive)
+                return;
+
             transform.position = Vector3.MoveTowards(transform.position, m_TargetPos, m_MoveSpeed * Time.deltaTime);
             GetTargetPosition();
         }
@@ -55,26 +64,49 @@ namespace Ghosts
         {
             if (other.CompareTag("Ghost"))
             {
-                var pos = transform.position;
-                if (m_Collider.GetColliderDirection(other.transform.position) == Vector3.right)
-                {
-                    m_TargetPos = new Vector3(m_MaxHorizontalMovement, pos.y, pos.z);
-                    m_SpriteRenderer.flipX = true;
-                }
-                else
-                {
-                    m_TargetPos = new Vector3(m_MinHorizontalMovement, pos.y, pos.z);
-                    m_SpriteRenderer.flipX = false;
-                }
+                ChangeTargetPositionX(m_Collider.GetColliderDirection(other.transform.position) == Vector3.right
+                    ? m_MaxHorizontalMovement
+                    : m_MinHorizontalMovement);
             }
 
-            if (!other.CompareTag("Player"))
-                return;
+            if (other.CompareTag("Player"))
+            {
+                var hitDirection = other.GetColliderDirection(transform.position);
+                var health = other.GetComponent<HealthObject>();
+                var movement = other.GetComponent<PlayerMovement>();
 
-            other.GetComponent<HealthObject>().Damage();
+                health.Damage();
+                movement.Rebound(hitDirection);
+            }
 
-            var colliderDirection = other.GetColliderDirection(transform.position);
-            other.GetComponent<PlayerMovement>().Rebound(colliderDirection);
+            DeactivateGhostOnHit();
+        }
+
+        /// <summary>
+        /// Change the target direction of the X-axis,
+        /// flip the sprite depending on the direction
+        /// </summary>
+        private void ChangeTargetPositionX(float x)
+        {
+            m_TargetPos = new Vector3(x, m_TargetPos.y, m_TargetPos.z);
+            m_SpriteRenderer.flipX = !(m_TargetPos.x < 2);
+        }
+
+        /// <summary>
+        /// Prevent ghost functionality and set death animation
+        /// </summary>
+        private void DeactivateGhostOnHit()
+        {
+            m_IsAlive = false;
+            m_Anim.SetTrigger(Death);
+        }
+
+        // Public method used as an Animation Event to destroy
+        // the gameObject once the death animation has finished
+        //
+        public void DestroyGhostAnimEvent()
+        {
+            Destroy(gameObject);
         }
     }
 }
