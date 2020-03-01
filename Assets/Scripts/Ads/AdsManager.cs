67763a77beda56e1682e09427085bbf1dba74576
LSO107 <34274877+LSO107@users.noreply.github.com>
2020-03-01 15:43:52 +0000

    Ad played on death

---
+++ Assets/Scripts/Ads/AdsManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.Advertisements;
+
+namespace Ads
+{
+    internal static class AdsManager
+    {
+        private const string GameId = "3490400";
+
+        private static float m_TimeTillNextAd;
+
+        public static void Initialize()
+        {
+            Advertisement.Initialize(GameId, true);
+        }
+
+        public static void ShowAd()
+        {
+            Debug.Log($"Current time is : {Time.time} and time till next ad is : {m_TimeTillNextAd}");
+
+            if (!CanShowAd() || Advertisement.isShowing)
+                return;
+
+            Advertisement.Show();
+            m_TimeTillNextAd = Time.time + 90;
+        }
+
+        private static bool CanShowAd()
+        {
+            return Time.time >= m_TimeTillNextAd;
+        }
+    }
+}
