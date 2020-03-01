4f161eeca89aa4b2e7d3a67f5cf75f37f1c99d2f
LSO107 <34274877+LSO107@users.noreply.github.com>
2020-03-01 17:14:04 +0000

    Hand Gesture Tutorial

---
+++ Assets/Scripts/UI/HandGestureTutorial.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace UI
+{
+    internal sealed class HandGestureTutorial : MonoBehaviour
+    {
+        private void Start()
+        {
+            var completeTutorial = PlayerPrefs.GetInt("CompleteTutorial", 0);
+
+            if (completeTutorial >= 1)
+            {
+                Destroy(gameObject);
+            }
+        }
+
+        public void CompleteTutorial()
+        {
+            PlayerPrefs.SetInt("CompleteTutorial", 1);
+            Destroy(gameObject);
+        }
+    }
+}
